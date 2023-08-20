6b56be55173cdc6cb292fa17c0a349243ff4d9ea
Sphere Dev <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-20 03:29:02 -0500

    Update index.ts

--- recurring/resume-pause/index.ts
+++ recurring/resume-pause/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/v1";
+const API_URL = "https://api.spherepay.co/v1";
 const API_KEY = process.env.API_KEY;
 const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
 console.log("API_KEY", config);
