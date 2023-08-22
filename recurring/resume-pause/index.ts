7a0a5a42aa0366a92b6c35553cfb5dfb8cf0c4d4
Sphere Laboratories Admin <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-22 15:19:04 -0500

    chore: lint

--- recurring/resume-pause/index.ts
+++ recurring/resume-pause/index.ts
@@ -5,7 +5,7 @@ const API_KEY = process.env.API_KEY;
 const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
 
 const updateSubscription = async (subscriptionId: string, status: string) => {
-  // A paused subscription will not be billed. 
+  // A paused subscription will not be billed.
   const response = await axios.post(
     `${API_URL}/subscription/${subscriptionId}`,
     {
@@ -22,15 +22,21 @@ const updateSubscription = async (subscriptionId: string, status: string) => {
 
   // 1. Pausing a Subscription
   //     To pause a subscription, update it's status to "paused".
-  //     A "paused" subscription will not be billed regardless of the subscription's "currentPeriodEnd". 
-  //     Note that "paused" subscriptions can be "resumed" by the merchant at anytime. 
+  //     A "paused" subscription will not be billed regardless of the subscription's "currentPeriodEnd".
+  //     Note that "paused" subscriptions can be "resumed" by the merchant at anytime.
 
-  console.log("pausedSubscription", await updateSubscription(subscriptionId, "paused"))
+  console.log(
+    "pausedSubscription",
+    await updateSubscription(subscriptionId, "paused"),
+  );
 
   // 2. Reactivating a Subscription
-  //    To resume (or reactivate) a subscription, update it's status to "active". 
-  //    An "active" subscription will be billed at the end of it's "currentPeriodEnd". 
-  //    Note that "active" subscriptions can be "paused" by the merchant at anytime. 
+  //    To resume (or reactivate) a subscription, update it's status to "active".
+  //    An "active" subscription will be billed at the end of it's "currentPeriodEnd".
+  //    Note that "active" subscriptions can be "paused" by the merchant at anytime.
 
-  console.log("activeSubscription", await updateSubscription(subscriptionId, "active"))
+  console.log(
+    "activeSubscription",
+    await updateSubscription(subscriptionId, "active"),
+  );
 })();
