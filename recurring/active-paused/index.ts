ef19f7062e34f78353766cf8acd5a7fa7285c001
Sphere Laboratories Admin <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-22 16:17:27 -0500

    Update index.ts

--- recurring/active-paused/index.ts
+++ recurring/active-paused/index.ts
@@ -4,6 +4,15 @@ const API_URL = "https://api.spherepay.co/v1";
 const API_KEY = process.env.API_KEY;
 const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
 
+const retrieveSubscription = async (subscriptionId: string) => {
+  const response = await axios.get(
+    `${API_URL}/subscription/${subscriptionId}`,
+    config,
+  );
+  const subscription = response.data.data.subscription;
+  return subscription;
+};
+
 const updateSubscription = async (subscriptionId: string, status: string) => {
   const response = await axios.post(
     `${API_URL}/subscription/${subscriptionId}`,
@@ -19,7 +28,10 @@ const updateSubscription = async (subscriptionId: string, status: string) => {
 (async () => {
   const subscriptionId = process.env.SUBSCRIPTION_ID;
 
-  // 1. Pausing a Subscription
+  const subscription = await retrieveSubscription(subscriptionId);
+
+  if (subscription.status == "active"){
+// 1. Pausing a Subscription
   //     To pause a subscription, update it's status to "paused".
   //     A "paused" subscription will not be billed regardless of the subscription's "currentPeriodEnd".
   //     Note that "paused" subscriptions can be "resumed" by the merchant at anytime.
@@ -28,14 +40,22 @@ const updateSubscription = async (subscriptionId: string, status: string) => {
     "pausedSubscription",
     await updateSubscription(subscriptionId, "paused"),
   );
-
-  // 2. Reactivating a Subscription
+  } else if (subscription.status == "paused"){
+// 2. Reactivating a Subscription
   //    To resume (or reactivate) a subscription, update it's status to "active".
   //    An "active" subscription will be billed at the end of it's "currentPeriodEnd".
   //    Note that "active" subscriptions can be "paused" by the merchant at anytime.
 
-  console.log(
-    "activeSubscription",
-    await updateSubscription(subscriptionId, "active"),
-  );
+  // console.log(
+  //   "activeSubscription",
+  //   await updateSubscription(subscriptionId, "active"),
+  // );
+  console.log("activeSubscription", await updateSubscription(subscriptionId, "active"));
+  } else {
+    console.log(`You cannot pause or active a subscription with status: ${subscription.status}`)
+  }
+
+  
+
+  
 })();
