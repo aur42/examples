669e73d9f56184fa4bacb4b02308049a8331df7f
Sphere Laboratories Admin <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-21 22:41:12 -0500

    Update index.ts

--- payouts/wallet-to-bankAccount/index.ts
+++ payouts/wallet-to-bankAccount/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const API_URL = "https://api.staging.spherepay.co/v1"
-const config = {headers: {'Authorization': 'Bearer secret_043de2ea0dd7406b8a11b67f283a27d4'}}
+const API_URL = "https://api.staging.spherepay.co/v1";
+const config = {
+  headers: { Authorization: "Bearer secret_043de2ea0dd7406b8a11b67f283a27d4" },
+};
 
 interface CreateBankAccount {
   bankName: string;
@@ -13,62 +15,50 @@ interface CreateBankAccount {
 }
 
 interface CreatePayout {
-  type: "walletToBankAccount"
+  type: "walletToBankAccount";
   fromWallet: string;
   sourceNetwork: "eth" | "tron";
   sourceCurrency: "usdt" | "usdc";
   destinationCurrency: "usd";
-  destinationNetwork: "ach" | "wire"
+  destinationNetwork: "ach" | "wire";
   toBankAccount: string;
   amount: string;
 }
 
-const createBankAccount = async(args: CreateBankAccount) => {
-  const response = axios.post(
-    `${API_URL}/bankAccount`,
-    args,
-    config,
-  ).then((response) => console.log(response.data)).catch((error) => console.log(error.response.data));
+const createBankAccount = async (args: CreateBankAccount) => {
+  const response = axios
+    .post(`${API_URL}/bankAccount`, args, config)
+    .then((response) => console.log(response.data))
+    .catch((error) => console.log(error.response.data));
   // if (response.status !== 200) {
   //   throw new Error(response.data);
   // }
   // return response.data.data.bankAccount;
   return null;
-}
+};
 
-
-const listBankAccount = async() => {
-  const response = await axios.get(
-    `${API_URL}/bankAccount`,
-    config,
-  )
-  if (response.status !== 200){
+const listBankAccount = async () => {
+  const response = await axios.get(`${API_URL}/bankAccount`, config);
+  if (response.status !== 200) {
     throw new Error(response.data);
   }
   return response.data.data.bankAccounts;
-}
+};
 
-const createPayout = async(args: CreatePayout) => {
-  const response = await axios.post(
-    `${API_URL}/payout`,
-    args,
-    config,
-  )
+const createPayout = async (args: CreatePayout) => {
+  const response = await axios.post(`${API_URL}/payout`, args, config);
   if (response.status !== 200) {
     throw new Error(response.data);
   }
   return response.data;
-}
+};
 
-(async() => {
+(async () => {
+  // const bankAccount = await createBankAccount({
+  //     accountType: "checking",
+  // });
+  // console.log(bankAccount)
 
-    // const bankAccount = await createBankAccount({
-    //     accountType: "checking",
-    // });
-    // console.log(bankAccount)
-
-    const bankAccounts = await listBankAccount();
-    console.log(bankAccounts)
-
-
-})()
+  const bankAccounts = await listBankAccount();
+  console.log(bankAccounts);
+})();
