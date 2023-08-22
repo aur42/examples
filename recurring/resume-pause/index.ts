3da227e96e80c58993fa7839de081ec4687c991c
Sphere Dev <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-22 15:17:40 -0500

    Update index.ts

--- recurring/resume-pause/index.ts
+++ recurring/resume-pause/index.ts
@@ -3,25 +3,13 @@ import axios from "axios";
 const API_URL = "https://api.spherepay.co/v1";
 const API_KEY = process.env.API_KEY;
 const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
-console.log("API_KEY", config);
 
-const resumeSubscription = async (subscriptionId: string) => {
+const updateSubscription = async (subscriptionId: string, status: string) => {
+  // A paused subscription will not be billed. 
   const response = await axios.post(
     `${API_URL}/subscription/${subscriptionId}`,
     {
-      status: "active",
-    },
-    config,
-  );
-  const subscription = response.data.data.subscription;
-  return subscription;
-};
-
-const pauseSubscription = async (subscriptionId: string) => {
-  const response = await axios.post(
-    `${API_URL}/subscription/${subscriptionId}`,
-    {
-      status: "paused",
+      status,
     },
     config,
   );
@@ -31,6 +19,18 @@ const pauseSubscription = async (subscriptionId: string) => {
 
 (async () => {
   const subscriptionId = process.env.SUBSCRIPTION_ID;
-  console.log("pausedSubscription", await pauseSubscription(subscriptionId));
-  console.log("resumedSubscription", await resumeSubscription(subscriptionId));
+
+  // 1. Pausing a Subscription
+  //     To pause a subscription, update it's status to "paused".
+  //     A "paused" subscription will not be billed regardless of the subscription's "currentPeriodEnd". 
+  //     Note that "paused" subscriptions can be "resumed" by the merchant at anytime. 
+
+  console.log("pausedSubscription", await updateSubscription(subscriptionId, "paused"))
+
+  // 2. Reactivating a Subscription
+  //    To resume (or reactivate) a subscription, update it's status to "active". 
+  //    An "active" subscription will be billed at the end of it's "currentPeriodEnd". 
+  //    Note that "active" subscriptions can be "paused" by the merchant at anytime. 
+
+  console.log("activeSubscription", await updateSubscription(subscriptionId, "active"))
 })();
