ea1e12c7605d67dc71920f618e02f2004b2e8122
Sphere Laboratories Admin <109333554+SphereLaboratories@users.noreply.github.com>
2023-08-23 00:58:00 -0500

    chore: added per-minute-billing

---
+++ webhook/express/basic-per-minute/script/index.ts
@@ -0,0 +1,103 @@
+import axios from "axios";
+
+const API_URL = "https://api.staging.spherepay.coß/v1";
+const API_KEY = process.env.API_KEY;
+const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
+
+const createProduct = async () => {
+  const response = await axios.post(
+    `${API_URL}/product`,
+    {
+      name: "Per Minute Subscription",
+    },
+    config,
+  );
+  const product = response.data.data.product;
+  return product;
+};
+
+const createPrice = async (productId: string) => {
+  const response = await axios.post(
+    `${API_URL}/price`,
+    {
+      product: productId,
+      type: "recurring",
+      recurring: {
+        type: "escrowed",
+        interval: "min",
+        intervalCount: 1,
+        usageType: "licensed",
+        defaultLength: 12,
+      },
+      currency: "bonk",
+      billingScheme: "perUnit",
+      name: "Basic Subscription Price",
+      description: "A simple description",
+      unitAmount: "5000000",
+    },
+    config,
+  );
+  const price = response.data.data.price;
+  return price;
+};
+
+const createCoupon = async () => {
+  const response = await axios.post(
+    `${API_URL}/coupon`,
+    {
+      name: "10% off",
+      percentOff: 10,
+      term: "oneTime",
+    },
+    config,
+  );
+  const coupon = response.data.data.coupon;
+  return coupon;
+};
+
+const createPromotionCode = async (couponId: string, code: string) => {
+  const response = await axios.post(
+    `${API_URL}/promotionCode`,
+    {
+      coupon: couponId,
+      code,
+    },
+    config,
+  );
+  const promotionCode = response.data.data.promotionCode;
+  return promotionCode;
+};
+
+const createPaymentLink = async (priceId: string, couponId: string) => {
+  const response = await axios.post(
+    `${API_URL}/paymentLink`,
+    {
+      coupon: couponId,
+      lineItems: [
+        {
+          price: priceId,
+          quantity: 1,
+          quantityMutable: false,
+        },
+      ],
+    },
+    config,
+  );
+  const paymentLink = response.data.data.paymentLink;
+  return paymentLink;
+};
+
+(async () => {
+  const code = "42";
+
+  const product = await createProduct();
+  const price = await createPrice(product.id);
+
+  const coupon = await createCoupon();
+  const promotionCode = await createPromotionCode(coupon.id, code);
+  console.log("coupon", coupon);
+  console.log("promotionCode", promotionCode);
+
+  const paymentLink = await createPaymentLink(price.id, coupon.id);
+  console.log("paymentLink", paymentLink.url);
+})();
---
+++ webhook/express/basic-per-minute/server/index.ts
@@ -0,0 +1,37 @@
+import bodyParser from "body-parser";
+import crypto from "crypto";
+import express from "express";
+const app = express();
+
+export const validPayload = (req: express.Request) => {
+  const body = req.body;
+  const headers = req.headers;
+  const signingSecret: string = process.env.SPHERE_WEBHOOK_SIGNING_SECRET || "";
+  const signature = crypto
+    .createHmac("sha256", Buffer.from(signingSecret))
+    .update(JSON.stringify(body), "utf-8")
+    .digest("hex");
+  if (headers["signature"] !== signature) {
+    return true
+  }
+  return false;
+}
+
+
+var jsonParser = bodyParser.json();
+
+app.post("/", jsonParser, async(req: express.Request, res: express.Response) => {
+
+  if (!validPayload(req)) {
+    res.status(500).json({message: "Invalid Payload"})
+    return
+  }  
+  
+  res.status(200).json({
+    message: "Message Received",
+  });
+});
+
+app.listen(8080, () => {
+  console.log(`⚪ Sphere basic-per-minute server :${8080}`);
+});
