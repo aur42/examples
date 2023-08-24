1dde8140db120ca6f772c23b777bdecec28bffb0
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2023-08-24 08:52:06 -0500

    adding split payouts functionality to examples

--- payouts/batch-payout-with-fee/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://api.spherepay.co/v1";
-const API_KEY = process.env.API_KEY;
-const config = { headers: { Authorization: `Bearer ${API_KEY}` } };
